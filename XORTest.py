376306c3bba4edb5bef626ed9a28e7f077393834
JosephBeasley <47218606+JosephBeasley@users.noreply.github.com>
2019-06-11 18:14:19 -0500

    Add files via upload

---
+++ XORTest.py
@@ -0,0 +1,12 @@
+def sxor(s1,s2):    
+    # convert strings to a list of character pair tuples
+    # go through each tuple, converting them to ASCII code (ord)
+    # perform exclusive or on the ASCII code
+    # then convert the result back to ASCII (chr)
+    # merge the resulting array of characters as a string
+    return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+
+printer=sxor('this is daddy','thisiskey')
+print(printer)
+deprinter=sxor(printer,'thisiskey')
+print(deprinter)
