e1fee44ff56c5b1a0f6c02bbaa16b8eb12de282e
JosephBeasley <47218606+JosephBeasley@users.noreply.github.com>
2019-06-12 09:31:17 -0500

    Update XORTest.py

--- XORTest.py
+++ XORTest.py
@@ -1,12 +1,103 @@
-def sxor(s1,s2):    
-    # convert strings to a list of character pair tuples
-    # go through each tuple, converting them to ASCII code (ord)
-    # perform exclusive or on the ASCII code
-    # then convert the result back to ASCII (chr)
-    # merge the resulting array of characters as a string
-    return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
-
-printer=sxor('this is daddy','thisiskey')
-print(printer)
-deprinter=sxor(printer,'thisiskey')
-print(deprinter)
+import re, socket
+keys = ['5V&$10jr@y', 'As26yx*uTB', '5V8tY7&ox^']
+​
+def main():
+​
+    returnMsg = []
+​
+    PORT = 3000
+​
+    HOST = "10.128.102.115"
+​
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+​
+    s.connect((HOST, PORT))
+​
+    received = s.recv(4096)
+​
+    print("Encoded data received: {}\n\n".format(received.decode()))
+​
+​
+    # receives data and splits it every 2 chars.
+​
+    data = received.decode('utf-8')
+​
+    data = re.findall('..', data)
+​
+    
+    # Iterates through keys
+​
+    for key in keys:
+​
+        keyConv = []
+​
+        msgConv = []
+​
+        text = []
+​
+        count = 0
+​
+        
+        # Converts msg into decimal format
+​
+        for d in data:
+​
+            msgConv.append(int(d, 16))
+​
+​
+        # Converts to decimal and appands
+​
+        for y in key:
+​
+            for x in y:
+​
+                keyConv.append(ord(x))
+​
+        
+        # XOR decimal representation of each char in key and msg
+​
+        # if key is shorter than msg, count + 1.  If count >= to 
+​
+        # key, set count back to 0
+​
+        for i in msgConv:
+​
+            key = keyConv[count]
+​
+            text.append(chr(i^key))
+​
+            count += 1
+​
+            if count >= len(keyConv):
+​
+                count = 0
+​
+​
+        returnMsg.append(''.join(text))
+​
+​
+    # A while loop to iterate through each converted message
+​
+    # Each msg is sent to the server to elicit a response
+​
+    z = 0
+​
+    while z < len(returnMsg):
+​
+        s.sendall(str(returnMsg[z]+'\n').encode())
+​
+        data = s.recv(4096)
+​
+        print(data.decode())
+​
+        z += 1
+​
+ 
+​
+    s.close()
+​
+​
+​
+if __name__ == "__main__":
+​
+    main()
